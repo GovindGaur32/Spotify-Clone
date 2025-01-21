080ce641dc13e48a0cbc3b18886ffd7d4486136c
GovindGaur32 <govindgaur327@gmail.com>
2025-01-21 21:50:59 +0530

    mute button fixed

--- spotify-clone/src/components/Player.jsx
+++ spotify-clone/src/components/Player.jsx
@@ -5,16 +5,29 @@ import { PlayerContext } from '../context/PlayerContext'
 const Player = (props) => {
 
   const { track, seekBar, seekBg, playStatus, play, pause, time, previous, next, seekSong } = useContext(PlayerContext);
-  // const [isMuted, setIsMuted] = useState(false);
+  const [isMuted, setIsMuted] = useState(false);
  
   let vol = document.getElementById('vol')
   let num = document.getElementById('num');
-  // var mute2 = document.getElementById('mute');
+  var mute2 = document.getElementById('mute');
   const volume = () => {
     props.ad.volume = vol.value / 100;
     num.innerHTML = vol.value;
   }
-  
+  function mute() { 
+    if (!isMuted) {
+      props.ad.muted = true;
+      mute2.src = assets.mute
+      num.innerHTML =0
+       setIsMuted(true);
+    } else {
+      props.ad.muted = false;
+      mute2.src = assets.volume_icon
+      num.innerHTML =50
+       setIsMuted(false);
+     }
+  }
+
   
 
   return track ? (
@@ -50,7 +63,7 @@ const Player = (props) => {
         <img className='w-4' src={assets.mic_icon} alt="" />
         <img className='w-4' src={assets.queue_icon} alt="" />
         <img className='w-4' src={assets.speaker_icon} alt="" />
-        <img  className='w-4 cursor-pointer' src={ assets.volume_icon} alt="" id='mute' />
+       <button onClick={mute}> <img  className='w-4 cursor-pointer' src={ assets.volume_icon} alt="" id='mute' /></button>
         <input type="range" name="volume" id="vol" onChange={volume} ></input><span id="num"></span>
         <img className='w-4' src={assets.mini_player_icon} alt="" />
         <img className='w-4' src={assets.zoom_icon} alt="" />
